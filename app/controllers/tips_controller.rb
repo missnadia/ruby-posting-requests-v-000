60d3d2d25e6e23ebf75d26acf627a7914baa82ee
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-24 21:08:18 +0000

    automatically backed up by learn

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,2 +1,10 @@
 class TipsController < ApplicationController
+  def create
+    resp = Faraday.post("http://api.foursquare.com/v2/tips/add") do |req|
+      req.params['oath_token'] = session[:token]
+      req.params['v'] = '20160201'
+      req.params['venueId'] = params[:venue_id]
+      req.params['text'] = params[:tip]
+    end
+  end
 end
